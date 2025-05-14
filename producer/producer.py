fbe295f00eabcc76b9a6f00375d731170b06b795
m-chevalier <29897178+m-chevalier@users.noreply.github.com>
2025-05-14 12:08:40 +0200

    Added prometheus

--- producer/producer.py
+++ producer/producer.py
@@ -1,35 +1,68 @@
 from confluent_kafka import Producer
 import time
 import string
+import asyncio
+import websockets
+from prometheus_client import Counter, Summary
+from prometheus_client import start_http_server
+start_http_server(8000)
 
 import os
+
+messages_processed = Counter('messages_processed_total', 'Total number of messages processed')
+messages_deleted = Counter('messages_deleted_total', 'Total number of messages deleted')
+process_duration = Summary('process_duration_seconds', 'Duration of message processing in seconds')
 
 KAFKA_OUTPUT_TOPIC = os.getenv('KAFKA_OUTPUT_TOPIC', 'llm-posts')
 KAFKA_BROKER = os.getenv('KAFKA_BROKER', 'kafka:9092')
 
+uri = "wss://jetstream2.us-east.bsky.network/subscribe?wantedCollections=app.bsky.feed.post"
 
 # Kafka Producer configuration
 conf = {
-    'bootstrap.servers': f'{KAFKA_BROKER}',  # Adjust with your Kafka broker address
-    'client.id': f'alphabet-producer'
+    'bootstrap.servers': f'{KAFKA_BROKER}',
+    'client.id': f'posts-producer'
 }
 
-# Initialize the Kafka Producer
+# Load allowed words from file
+with open('allowed-list.txt', 'r') as f:
+    allowed_words = set(f.read().splitlines())
+
 producer = Producer(conf)
 
-# Function to send message to Kafka
+
+# Callback function to handle delivery reports
 def delivery_report(err, msg):
     if err is not None:
+        #TODO: better logging
         print(f"Message delivery failed: {err}")
-    else:
-        print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+    
 
-# Send each letter of the alphabet to a Kafka topic
-topic = f'{KAFKA_OUTPUT_TOPIC}'
-for letter in string.ascii_lowercase:  # Loop through 'a' to 'z'
-    producer.produce(topic, letter.encode('utf-8'), callback=delivery_report)
-    producer.poll(0)  # Poll to trigger delivery report
-    time.sleep(1)  # Wait for 1 second
+async def listen_to_websocket():
+    async with websockets.connect(uri) as websocket:
+        while True:
+            try:
+                message = await websocket.recv()
+                start = time.time()
+                # Check if the message contains at least one allowed word
+                if any(word in message for word in allowed_words):
+                    producer.produce(f"{KAFKA_OUTPUT_TOPIC}", message.encode('utf-8'), callback=delivery_report)
+                    producer.poll(0)  # Poll to trigger delivery report
+                    messages_processed.inc()
+                else:
+                    messages_deleted.inc()
+
+            except websockets.ConnectionClosed as e:
+                print(f"Connection closed: {e}")
+                break
+            except Exception as e:
+                print(f"Error: {e}")
+            finally:
+                end = time.time()
+                process_duration.observe(end - start)
+
+
+asyncio.get_event_loop().run_until_complete(listen_to_websocket())
 
 # Wait for any remaining messages to be delivered
 producer.flush()
